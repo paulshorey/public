0036e4599ea5632dc8708875a525c45f5055e971
Paul Shorey <ps@artspaces.net>
2020-06-24 23:46:46 -0700

    new build

--- fe/nlp.domains/component---src-pages-404-js-68f0c872a14ebbd780a6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(t,o,e){"use strict";e.r(o);var n=e("q1tI"),r=e.n(n),p=e("uVCN");var a=function(t){var o,e;function n(){return t.apply(this,arguments)||this}return e=t,(o=n).prototype=Object.create(e.prototype),o.prototype.constructor=o,o.__proto__=e,n.prototype.render=function(){return r.a.createElement(p.a,{location:this.props.location},r.a.createElement("p",null,"Page Not Found"))},n}(r.a.Component);o.default=a}}]);
-//# sourceMappingURL=component---src-pages-404-js-68f0c872a14ebbd780a6.js.map
--- fe/nlp.domains/component---src-pages-domains-js-62ad316c36ce42ccd476.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{Hnhx:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),a=t.n(o),c=t("YwZP");e.default=function(){return a.a.createElement(c.Redirect,{to:"/domain"})}}}]);
-//# sourceMappingURL=component---src-pages-domains-js-62ad316c36ce42ccd476.js.map
