a4f791b8f0ac62e5615fee01476670d273fbd096
Paul Shorey <ps@artspaces.net>
2020-10-10 23:50:19 -0500

    ps mailchimp

---
+++ ps/assets/js/mailchimp.js
@@ -0,0 +1,11 @@
+!(function (c, h, i, m, p) {
+  ;(m = c.createElement(h)),
+    (p = c.getElementsByTagName(h)[0]),
+    (m.async = 1),
+    (m.src = i),
+    p.parentNode.insertBefore(m, p)
+})(
+  document,
+  "script",
+  "https://chimpstatic.com/mcjs-connected/js/users/e61d6dc39b600192579a1b7b3/08379f53c76e9e2a7eecdf886.js"
+)
