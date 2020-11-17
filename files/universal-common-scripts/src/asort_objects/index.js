cca1663e9f8129e7e5622f051cf9b5869d4e123e
Paul Shorey <ps@artspaces.net>
2020-11-17 16:17:48 -0600

    fe

--- files/universal-common-scripts/src/asort_objects/index.js
+++
@@ -1,7 +0,0 @@
-import asort_objects_by_property from './asort_by_property.js'
-import asort_objects_by_property_and_position from './asort_by_property_and_position.js'
-
-export {
-  asort_objects_by_property,
-  asort_objects_by_property_and_position,
-}
--- files/universal-common-scripts/src/promises.js
+++
@@ -1,9 +0,0 @@
-export const sleep = function(ms = 0) {
-  return new Promise(r => setTimeout(r, ms))
-}
-
-export const forEachAsync = async function(array, fn) {
-  for (let t of array) {
-    await fn(t)
-  }
-}
