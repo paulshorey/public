e4bb4993b26b56a9942685ea368ce872c966d6fb
Paul Shorey <ps@artspaces.net>
2020-12-02 16:42:41 -0600

    ps

--- ps/assets/js/uuiCarousel/uuiCarousel.js
+++ ps/assets/js/uuiCarousel/uuiCarousel.js
@@ -1,32 +1,42 @@
-// setTimeout(function () {
-$(document).ready(function () {
-  $(".uuiCarousel").each(function () {
-    let $carousel = this
-    let $prev = $carousel.find(".prev")
-    let $next = $carousel.find(".next")
-    let $images = $carousel.find(".images")
-    $next.click(function () {
-      setTimeout(function () {
-        $prev.css("opacity", "1")
-        $prev.css("visibility", "visible")
-      }, 500)
-      $images.animate(
-        {
-          scrollLeft: $images.scrollLeft() + $images.width()
-        },
-        500,
-        "linear"
-      )
+;(function () {
+  function init() {
+    $(document).ready(function () {
+      $(".uuiCarousel").each(function () {
+        let $carousel = this
+        let $prev = $carousel.find(".prev")
+        let $next = $carousel.find(".next")
+        let $images = $carousel.find(".images")
+        $next.click(function () {
+          setTimeout(function () {
+            $prev.css("opacity", "1")
+            $prev.css("visibility", "visible")
+          }, 500)
+          $images.animate(
+            {
+              scrollLeft: $images.scrollLeft() + $images.width()
+            },
+            500,
+            "linear"
+          )
+        })
+        $prev.click(function () {
+          $images.animate(
+            {
+              scrollLeft: $images.scrollLeft() - $images.width()
+            },
+            500,
+            "linear"
+          )
+        })
+      })
     })
-    $prev.click(function () {
-      $images.animate(
-        {
-          scrollLeft: $images.scrollLeft() - $images.width()
-        },
-        500,
-        "linear"
-      )
-    })
-  })
-})
-// }, 3000)
+  }
+  // production
+  try {
+    init()
+  } catch (e) {
+    //  "$" not ready
+  }
+  // dev
+  setTimeout(init, 2000)
+})()
