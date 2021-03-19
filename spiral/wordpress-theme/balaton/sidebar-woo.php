675da156ed7645207a8807ac52f223371bf487c9
Paul Shorey <ps@artspaces.net>
2021-03-19 15:27:35 -0500

    signup app

--- spiral/wordpress-theme/balaton/sidebar-woo.php
+++
@@ -1,7 +0,0 @@
-<?php if ( is_active_sidebar( 'sidebar-woocommerce' ) ) { ?>
-  <div class="sidebar col-lg-3 l-1 border-left">
-    <div class="sidebar-inner">
-      <?php dynamic_sidebar( 'sidebar-woocommerce' ); ?>
-    </div>
-  </div>
-<?php } ?>
