5d9fc263adf4210cef4f6b233c45e5c8c05ac8bd
Paul Shorey <ps@artspaces.net>
2021-03-21 00:23:56 -0500

    logo

--- spiral/wordpress-theme/balaton/archive.php
+++
@@ -1,6 +0,0 @@
-<?php get_header();
-
-weart_sectionTitle(array('title'=> get_the_archive_title() ));
-weart_section(array('layout'=>'layout_5','on'=>1));
-
-get_footer() ?>
--- spiral/wordpress-theme/balaton/search.php
+++
@@ -1,6 +0,0 @@
-<?php get_header();
-
-weart_sectionTitle(array('title'=>esc_html__( 'Searched: ', 'balaton' ) . get_search_query()  ));
-weart_section(array('layout'=>'layout_5','on'=>1));
-
-get_footer() ?>
